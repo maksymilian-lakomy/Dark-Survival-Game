94ea78f8014109fd860d6b79b61c75cf66b2628c
sadblondeboi <bezawka@gmail.com>
2019-11-30 19:04:40 +0100

    inventory functions

--- Assets/Scripts/Items/Inventory.cs
+++ Assets/Scripts/Items/Inventory.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿﻿﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using BehaviorInterfaces;
+using ItemInterfaces;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
-{
+public class Inventory : MonoBehaviour {
+    
     #region Singleton
 
     public static Inventory instance;
@@ -24,17 +25,38 @@ public class Inventory : MonoBehaviour
 
     #endregion
 
-    public List<ItemData> items = new List<ItemData>();
+    public List<IInventoryItem> inventoryItems = new List<IInventoryItem>();
+    public IItem equippedItem; // interfejs, bo mozna w łapce trzymac na przyklad kurczaka, ktory jest
+                               // MonoBehaviour a nie InventoryItemData (to samo z ekwipunkiem)
     public int Space;
 
-    public bool AddItem(ItemData item)
-    {
-        if (items.Count >= Space)
-        {
+    public bool AddItem(IInventoryItem item) {
+        if (inventoryItems.Count >= Space) {
             Debug.Log("Not enough room.");
             return false;
         }
-        items.Add(item);
+        inventoryItems.Add(item);
         return true;
+    }
+
+    public bool AddItem(IItem item) {
+        if (equippedItem == null) {
+            // equippedItem = ten item na którym jest focus
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void DropEquippedItem() {
+        if (equippedItem != null)
+        {
+            ItemData itemData = equippedItem as ItemData;
+            if (itemData != null)
+            {
+                WorldItem.CreateWorldItem(itemData);
+            }
+            equippedItem = null;
+        }
     }
 }
