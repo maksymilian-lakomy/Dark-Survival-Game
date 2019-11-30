8f653ce9b249cc049139bd7e60cfd3a2c8f44c0e
Maksymilian Łakomy <zommer128@gmail.com>
2019-11-30 19:10:14 +0100

    Player has a vision now

--- Assets/Scripts/PlayerVision.cs
+++ Assets/Scripts/PlayerVision.cs
@@ -1,17 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class PlayerVision : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace DefaultNamespace {
+    public class PlayerVision: MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
+
+        [SerializeField] private Vector3 eyesPosition;
+        [SerializeField] private float viewDistance = 1f;
+
+        [SerializeField] private GameObject currentActive;
         
+        
+        private void FixedUpdate() {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + eyesPosition, transform.right * viewDistance);
+            if (hit.collider != null) {
+                GameObject newActive = hit.collider.gameObject;
+                if (newActive != currentActive) {
+                    currentActive = newActive;
+                }
+            }
+            Debug.DrawRay(transform.position + eyesPosition, transform.right*viewDistance, Color.red);
+        }
+
+
     }
 }
